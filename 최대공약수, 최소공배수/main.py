51d505041d38317bcfa6c24934b350095bca43fe
syh382 <tojonggo@gmail.com>
2026-06-06 15:55:41 +0900

    math 미사용버전

---
+++ 최대공약수, 최소공배수/main.py
@@ -0,0 +1,20 @@
+n = int(input())
+lis = list(map(int, input().split()))
+f,s = 0,0
+for i in range(min(lis),0,-1):
+    for j in lis:
+        if j % i:
+            break
+    else:
+        f = i
+        break
+num = 0
+max = max(lis)
+while not s:
+    num+=1
+    for i in lis:
+        if max*num % i:
+            break
+    else:
+        s = max*num
+print(f,s)
